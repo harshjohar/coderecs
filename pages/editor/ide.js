1a3d82c3e8e2f80d8a5a13f28121efb9c6be7827
Harsh Johar <hpsj3112@gmail.com>
2022-06-01 05:22:08 +0530

    ide page setup

--- pages/editor/ide.js
+++ pages/editor/ide.js
@@ -1,9 +1,177 @@
-import React from 'react'
+import Head from "next/head";
+import React, { useEffect, useState } from "react";
 
-function ide() {
-  return (
-    <div>ide</div>
-  )
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import { javascriptDefault } from "../../constants/javascriptDefault";
+import { languageOptions } from "../../constants/languageOptions";
+import useKeyPress from "../../hooks/useKeyPress";
+import { defineTheme } from "../../lib/defineTheme";
+
+function Ide() {
+    const [code, setCode] = useState(javascriptDefault);
+    const [customInput, setCustomInput] = useState("");
+    const [outputDetails, setOutputDetails] = useState(null);
+    const [processing, setProcessing] = useState(null);
+    const [theme, setTheme] = useState("cobalt");
+    const [language, setLanguage] = useState(languageOptions[0]);
+
+    const enterPress = useKeyPress("Enter");
+    const ctrlPress = useKeyPress("Control");
+
+    const onSelectChange = (sl) => {
+        console.log("selected Option...", sl);
+        setLanguage(sl);
+    };
+
+    useEffect(() => {
+        if (enterPress && ctrlPress) {
+            console.log("enterPress", enterPress);
+            console.log("ctrlPress", ctrlPress);
+            handleCompile();
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [ctrlPress, enterPress]);
+
+    const onChange = (action, data) => {
+        switch (action) {
+            case "code": {
+                setCode(data);
+                break;
+            }
+            default: {
+                console.warn("case not handled!", action, data);
+            }
+        }
+    };
+
+    const handleCompile = () => {
+        setProcessing(true);
+        const formData = {
+            language_id: language.id,
+            // encode source code in base64
+            source_code: btoa(code),
+            stdin: btoa(customInput),
+        };
+        const options = {
+            method: "POST",
+            url: process.env.REACT_APP_RAPID_API_URL,
+            params: { base64_encoded: "true", fields: "*" },
+            headers: {
+                "content-type": "application/json",
+                "Content-Type": "application/json",
+                "X-RapidAPI-Host": process.env.REACT_APP_RAPID_API_HOST,
+                "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+            },
+            data: formData,
+        };
+
+        axios
+            .request(options)
+            .then(function (response) {
+                console.log("res.data", response.data);
+                const token = response.data.token;
+                checkStatus(token);
+            })
+            .catch((err) => {
+                let error = err.response ? err.response.data : err;
+                // get error status
+                let status = err.response.status;
+                console.log("status", status);
+                if (status === 429) {
+                    console.log("too many requests", status);
+
+                    showErrorToast(
+                        `Quota of 100 requests exceeded for the Day! Please read the blog on freeCodeCamp to learn how to setup your own RAPID API Judge0!`,
+                        10000
+                    );
+                }
+                setProcessing(false);
+                console.log("catch block...", error);
+            });
+    };
+
+    const checkStatus = async (token) => {
+        const options = {
+            method: "GET",
+            url: process.env.REACT_APP_RAPID_API_URL + "/" + token,
+            params: { base64_encoded: "true", fields: "*" },
+            headers: {
+                "X-RapidAPI-Host": process.env.REACT_APP_RAPID_API_HOST,
+                "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+            },
+        };
+        try {
+            let response = await axios.request(options);
+            let statusId = response.data.status?.id;
+
+            // Processed - we have a result
+            if (statusId === 1 || statusId === 2) {
+                // still processing
+                setTimeout(() => {
+                    checkStatus(token);
+                }, 2000);
+                return;
+            } else {
+                setProcessing(false);
+                setOutputDetails(response.data);
+                showSuccessToast(`Compiled Successfully!`);
+                console.log("response.data", response.data);
+                return;
+            }
+        } catch (err) {
+            console.log("err", err);
+            setProcessing(false);
+            showErrorToast();
+        }
+    };
+
+    function handleThemeChange(th) {
+        const theme = th;
+        console.log("theme...", theme);
+
+        if (["light", "vs-dark"].includes(theme.value)) {
+            setTheme(theme);
+        } else {
+            defineTheme(theme.value).then((_) => setTheme(theme));
+        }
+    }
+    useEffect(() => {
+        defineTheme("oceanic-next").then((_) =>
+            setTheme({ value: "oceanic-next", label: "Oceanic Next" })
+        );
+    }, []);
+
+    const showSuccessToast = (msg) => {
+        toast.success(msg || `Compiled Successfully!`, {
+            position: "top-right",
+            autoClose: 1000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+        });
+    };
+    const showErrorToast = (msg, timer) => {
+        toast.error(msg || `Something went wrong! Please try again.`, {
+            position: "top-right",
+            autoClose: timer ? timer : 1000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+        });
+    };
+    return (
+        <div>
+            <Head>
+                <title>IDE</title>
+            </Head>
+            <p>hello</p>
+        </div>
+    );
 }
-
-export default ide
+export default Ide;
